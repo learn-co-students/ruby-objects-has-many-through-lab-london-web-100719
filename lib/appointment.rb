cb657ccf326dbb6335c3a57a8e48a2e09ce5b761
Peter <ayenicology@gmail.com>
2019-10-15 01:05:13 +0100

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all = []
+
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
